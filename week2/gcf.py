7710c2281ed30270258b4d24b417e510e828cfd7
Jay Manjrekar <89176673+JayManjrekar@users.noreply.github.com>
2022-03-31 18:18:27 +0000

    Added a GCF math function

---
+++ week2/gcf.py
@@ -0,0 +1,25 @@
+class mathFunc:
+  def __init__(self,firstnumber,secondnumber):
+    self.firstnumber=firstnumber
+    self.secondnumber=secondnumber
+    
+  def findlcm(self):
+      if (self.firstnumber > self.secondnumber):
+          maximum = self.firstnumber
+      else:
+          maximum = self.secondnumber
+      while (True):
+          if (maximum % self.firstnumber == 0 and maximum % self.secondnumber == 0):
+              break
+          maximum = maximum + 1
+      return maximum
+
+  def __call__(self):
+    ans = self.findlcm()
+    print("\n Least Common Multiple of {0} and {1} is: {2}".format(self.firstnumber, self.secondnumber, ans))
+    print()
+    
+
+
+math = mathFunc(23,10013)
+math()
